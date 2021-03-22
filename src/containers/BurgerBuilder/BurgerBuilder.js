67a7bf5246dbb8022798832c39e2680b91b98604
Ee-Hsin <eehsinkok777@gmail.com>
2021-03-22 20:26:31 +0800

    Improved performance by optimizing with preventing unecessary re-renders, and changed folder structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
@@ -94,7 +94,12 @@ class BurgerBuilder extends Component {
         return(
         <Aux>
             <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                <OrderSummary price={this.state.totalPrice} purchaseCancelled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler} ingredients={this.state.ingredients}/>
+                <OrderSummary 
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice} 
+                purchaseCancelled={this.purchaseCancelHandler} 
+                purchaseContinued={this.purchaseContinueHandler} 
+                />
             </Modal>
             <Burger ingredients = {this.state.ingredients}/>
             <BuildControls
